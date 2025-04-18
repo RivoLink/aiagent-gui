ccb24029f4c214308bdd271168e106da4c584335
RivoLink <rivo.link@gmail.com>
2025-04-19 01:57:23 +0300

    setup markdown renderer

--- src/components/common/Message/Message.jsx
+++ src/components/common/Message/Message.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import Markdown from "react-markdown";
+
 import { formatHM } from '../../../assets/utils/time';
 
 import './Message.css';
@@ -13,7 +15,9 @@ function Message({ group, message }) {
                 <div className="message-container">
                     <div className="avatar"></div>
                     <div className="message received" onClick={toggleShowInfo}>
-                        <div className="message-content">{message.text}</div>
+                        <div className="message-content">
+                            <Markdown>{message.text}</Markdown>
+                        </div>
                         {showInfo && (
                             <div className="message-time">{formatHM(message.date)}</div>
                         )}
@@ -22,7 +26,9 @@ function Message({ group, message }) {
             ) : (
                 <div className="message-container sent">
                     <div className="message sent" onClick={toggleShowInfo}>
-                        <div className="message-content">{message.text}</div>
+                        <div className="message-content">
+                            <Markdown>{message.text}</Markdown>
+                        </div>
                         {showInfo && (
                             <div className="message-time">{formatHM(message.date)}</div>
                         )}
