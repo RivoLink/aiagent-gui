5c479c56cef901715be838c2465628c935c8152c
RivoLink <rivo.link@gmail.com>
2025-05-02 23:45:22 +0300

    add command newsession

--- src/core/Runnable.js
+++ src/core/Runnable.js
@@ -1,3 +1,4 @@
+import AiAgent from "./AiAgent";
 import { formatHM, formatWM } from "@app/assets/utils/time";
 
 export default class Runnable {
@@ -8,6 +9,15 @@ export default class Runnable {
 
     static runnable_retore(_, callback) {
         callback('Messages restored.');
+    }
+
+    static runnable_newsession(_, callback) {
+        AiAgent.Session.refresh();
+        callback('New session start.');
+    }
+
+    static runnable_new(_, callback) {
+        Runnable.runnable_newsession(_, callback);
     }
 
     static runnable_today(_, callback) {
